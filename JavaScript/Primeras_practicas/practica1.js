01a296c80acf9baa9b9dca395172e3830e3be5ea
Flora M <floramaldonado14@gmail.com>
2022-02-09 17:59:04 -0600

    agregando primera practica

---
+++ JavaScript/Primeras_practicas/practica1.js
@@ -0,0 +1,16 @@
+//Variables y operadores
+
+var a = 1;
+let b = 45;
+var c = 6;
+var d;
+
+d = a + b;
+
+console.log(d)
+console.log("Resultado de la variable: ",d)
+console.log(a+b)
+console.log(a,b)
+
+//, para números
+//+texto, es un ejemplo de como podríamos diferenciarlo
